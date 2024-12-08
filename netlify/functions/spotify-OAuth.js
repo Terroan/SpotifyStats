03df575b5c13cfad7cbeb1bfc8c2c2dcd70943ee
DAJEAtSchool <daniel.jessner@htl-saalfelden.at>
2024-12-08 19:11:21 +0100

    Update spotify-OAuth.js

--- netlify/functions/spotify-OAuth.js
+++ netlify/functions/spotify-OAuth.js
@@ -1,19 +1,19 @@
 const querystring = require('querystring');
 
-export const handler = async (event, context) => {
+export const handler = async function (event, context) {
   const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
-  const SPOTIFY_REDIRECT_URI = process.env.SPOTIFY_REDIRECT_URI;
+  const SPOTIFY_REDIRECT_URI = process.env.SPOTIFY_REDIRECT_URI; // Diese URI sollte bereits korrekt in deiner .env-Datei oder Umgebungsvariable definiert sein
   const SPOTIFY_SCOPES = "user-top-read playlist-modify-public";
 
   // Spotify Auth URL
   const spotifyAuthUrl = `https://accounts.spotify.com/authorize?${querystring.stringify({
     client_id: SPOTIFY_CLIENT_ID,
     response_type: 'code',
-    redirect_uri: SPOTIFY_REDIRECT_URI,
+    redirect_uri: SPOTIFY_REDIRECT_URI, // Achte darauf, dass du hier keinen encodeURIComponent verwendest
     scope: SPOTIFY_SCOPES,
   })}`;
 
-  // Rückgabe einer Weiterleitung (302: Temporäre Umleitung)
+  // Weiterleitung an die Auth-URL
   return {
     statusCode: 301,
     headers: {
@@ -21,4 +21,3 @@ export const handler = async (event, context) => {
     },
   };
 };
-
